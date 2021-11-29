bca3ebec21274fd28c3ad4135bde52418f91d97d
manufarez <manufarez@gmail.com>
2021-11-29 19:04:36 +0100

    Changed asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = 'http://www.manufarez.com'
+  # config.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -39,6 +39,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
+  config.action_controller.asset_host = 'https://www.manufarez.com'
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil

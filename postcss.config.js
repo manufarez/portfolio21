4c0cd380b1d08321a7d0f44defc282c015dfe02c
manufarez <manufarez@gmail.com>
2021-12-02 15:37:00 +0100

    Second try

--- postcss.config.js
+++ postcss.config.js
@@ -19,10 +19,9 @@ if (process.env.RAILS_ENV === "production") {
     require('@fullhuman/postcss-purgecss')({
       content: [
         './app/**/*.html.erb',
+        './app/**/**/*.html.erb',
         './app/helpers/**/*.rb',
         './app/javascript/**/*.js',
-        './app/javascript/**/*.vue',
-        './app/javascript/**/*.jsx',
       ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })

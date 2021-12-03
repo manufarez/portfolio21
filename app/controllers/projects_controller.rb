a2931018e6284804750db1025f0f75a24331ff31
manufarez <manufarez@gmail.com>
2021-12-03 01:02:27 +0100

    auth user for projetcs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
 
   # GET /projects or /projects.json
   def index
